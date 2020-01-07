f3c89579d0e11ecf945f9a49f4a9fd7e05ad15a8
Antony Sanders <antony.devon92@gmail.com>
2020-01-07 21:28:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :word 
+  
+  @@array = []
+  
+  def initialize(word)
+    #binding.pry
+    @word = word
+    @word
+  end 
+  
+  def match_word
+    
+  end 
+  
+  
+  
+end 
